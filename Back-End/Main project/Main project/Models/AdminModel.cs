94f110d5b60b776bbf10a3c688953bec81c0ec39
Akilaudantha <127245719+Akilaudantha@users.noreply.github.com>
2024-10-30 06:22:12 +0530

    Delete Back-End/Main project directory

--- Back-End/Main project/Main project/Models/AdminModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Main_project.Models
-{
-    public class AdminModel
-    {
-        public string Username { get; set; }
-        public string Password { get; set; } // Store the password in plain text (not recommended)
-    }
-}
--- Back-End/Main project/Main project/Models/ViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Main_project.Models
-{
-    public class ViewModel
-    {
-        public int Id { get; set; }       // User ID
-        public string Username { get; set; } // Username
-    }
-}
